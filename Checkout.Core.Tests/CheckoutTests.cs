6f4222200ca70bb04b1c19a9c862e6c6e3d4a170
p10271525 <p10271525@CTEDT00411>
2017-06-09 09:46:44 +0100

    Add project files.

---
+++ Checkout.Core.Tests/CheckoutTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Checkout.Core.Tests
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Test fixture for checkout.
+    /// </summary>
+    [TestFixture]
+    public class CheckoutTests
+    {
+        /// <summary>
+        /// Called before each test is run.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void ExampleTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Checkout/ICheckout.cs
@@ -0,0 +1,20 @@
+﻿namespace Checkout.Core
+{
+    /// <summary>
+    /// Checkout interface.
+    /// </summary>
+    interface ICheckout
+    {
+        /// <summary>
+        /// Scans the specified item.
+        /// </summary>
+        /// <param name="item">The name of scanned item.</param>
+        void Scan(string item);
+
+        /// <summary>
+        /// Gets the total price.
+        /// </summary>
+        /// <returns>Returns the total price as a whole number.</returns>
+        int GetTotalPrice();
+    }
+}
