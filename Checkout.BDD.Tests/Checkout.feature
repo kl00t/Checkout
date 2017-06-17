34d5065654040046b21cf3a9b52aa36c2070271b
Scott Vaughan <scott_vaughan@hotmail.com>
2017-06-17 11:28:40 +0100

    Add first test scenario

---
+++ Checkout.BDD.Tests/Checkout.feature
@@ -0,0 +1,11 @@
+﻿Feature: Checkout
+	Feature to respresent a checkout scanner and the ability to calculate the total price of all the items.
+
+@Ignore
+Scenario: Calculate the total price of a scanned item
+	Given I have a checkout system
+	And I scan an item using the checkout
+	When I calculate the total price
+	Then the totalprice should be correct
+	| item | unitprice | totalprice |
+	| A    | 9.99      | 9.99       |
---
+++ Checkout.BDD.Tests/CheckoutSteps.cs
@@ -0,0 +1,75 @@
+﻿using Checkout.Data;
+using Checkout.Core;
+using TechTalk.SpecFlow;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Checkout.BDD.Tests
+{
+    [Binding]
+    public class CheckoutSteps
+    {
+		/// <summary>
+		/// The checkout.
+		/// </summary>
+		private static Core.Checkout _checkout;
+
+		private static Mock<IProductRepository> _mockProductRepository;
+
+		private static Mock<ICarrierBag> _mockCarrierBag;
+
+		[BeforeScenario]
+		public void BeforeScenario()
+		{
+			_mockProductRepository = new Mock<IProductRepository>();
+			_mockCarrierBag = new Mock<ICarrierBag>();
+			_mockCarrierBag.Setup(x => x.CalculateBagCharge(It.IsAny<int>())).Returns(0);
+
+			_mockProductRepository.Setup(x => x.GetProductBySkuCode("A"))
+				.Returns(new Product
+				{
+					Sku = "A",
+					UnitPrice = 9.99m,
+					Description = "Pineapple",
+					SpecialOffer = new SpecialOffer
+					{
+						IsAvailable = true,
+						Quantity = 3,
+						Discount = 20
+					}
+				});
+		}
+
+		[AfterFeature]
+		public static void AfterFeature()
+		{
+			// run after the test have run
+		}
+
+		[Given(@"I have a checkout system")]
+        public static void GivenIHaveACheckoutSystem()
+        {
+			_checkout = new Core.Checkout(_mockProductRepository.Object, _mockCarrierBag.Object);
+        }
+
+		[Given(@"I scan an (.*) using the checkout")]
+		public void GivenIScanAnUsingTheCheckout(string item)
+		{
+			_checkout.Scan(item);
+		}
+
+		[When(@"I calculate the total price")]
+        public void WhenICalculateTheTotalPrice()
+        {
+			var totalPrice = _checkout.GetTotalPrice();
+			ScenarioContext.Current.Add("TotalPrice", totalPrice);
+		}
+
+		[Then(@"the (.*) should be correct")]
+		public void ThenTheShouldBeCorrect(string totalprice)
+		{
+			Assert.AreEqual(Convert.ToDecimal(totalprice), _checkout.TotalPrice);
+		}
+	}
+}
